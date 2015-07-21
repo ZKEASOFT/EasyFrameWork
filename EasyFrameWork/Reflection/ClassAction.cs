f0b4f45437243996890a07e362294cfb48724d54
Wayne <seriawei@gmail.com>
2015-07-21 11:58:35 +0800

    .

--- EasyFrameWork/Reflection/ClassAction.cs
+++ EasyFrameWork/Reflection/ClassAction.cs
@@ -18,7 +18,7 @@ namespace Easy.Reflection
         /// <returns>返回字典</returns>
         public static Dictionary<string, object> GetPropertieValues<T>(T obj)
         {
-            Type obj_Type = obj.GetType();
+            Type obj_Type = typeof(T);
             var properties = obj_Type.GetProperties();
             Dictionary<string, object> values = new Dictionary<string, object>();
             foreach (var item in properties)
@@ -39,7 +39,7 @@ namespace Easy.Reflection
         /// <returns>属性值</returns>
         public static object GetPropertyValue<T>(T item, string property)
         {
-            Type entityType = Easy.Loader.GetType<T>();
+            Type entityType = Loader.GetType<T>();
             PropertyInfo proper = entityType.GetProperty(property);
             if (proper != null && proper.CanRead)
             {
@@ -68,7 +68,7 @@ namespace Easy.Reflection
         /// <param name="value"></param>
         public static void SetPropertyValue<T>(T item, string property, object value)
         {
-            Type entityType = Easy.Loader.GetType<T>();
+            Type entityType = Loader.GetType<T>();
             PropertyInfo proper = entityType.GetProperty(property);
             if (proper != null && proper.CanWrite)
             {
@@ -96,11 +96,11 @@ namespace Easy.Reflection
         {
             if (data == null || data.Rows.Count == 0)
                 return default(T);
-            Type ty = Easy.Loader.GetType<T>();
+            Type ty = Loader.GetType<T>();
             T obj;
             if (ty.IsClass && ty.Name != "String")
             {
-                obj = Activator.CreateInstance<T>();
+                obj = Loader.CreateInstance<T>();
                 var properties = ty.GetProperties();
                 if (properties.Any())
                 {
